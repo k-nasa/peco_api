750823ec9cffdc10f1d8dc098ffd8adde4c6672b
k-nasa <htilcs1115@gmail.com>
2018-11-24 21:44:15 +0900

    Add hello world test code

--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,18 @@ fn rocket() -> Rocket {
 fn main() {
     rocket().launch();
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::Client;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("Hello, world!".into()));
+    }
+}
