5b8e1c8e44c5c5b71b5eaee02929e5f392dc118a
k-nasa <htilcs1115@gmail.com>
2018-11-24 21:42:31 +0900

    Add hello,world Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use crate::rocket::Rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+fn rocket() -> Rocket {
+    rocket::ignite().mount("/", routes![hello])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
