1774fac045fc5e7e13fce1820ac726da523b8cfb
k-nasa <htilcs1115@gmail.com>
2018-11-26 18:21:21 +0900

    $diesel migration run

--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,13 @@
 table! {
+    fixed_phrases (id) {
+        id -> Int4,
+        user_id -> Int4,
+        yes_text -> Text,
+        no_text -> Text,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         token -> Varchar,
@@ -6,3 +15,10 @@ table! {
         password_digest -> Varchar,
     }
 }
+
+joinable!(fixed_phrases -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    fixed_phrases,
+    users,
+);
