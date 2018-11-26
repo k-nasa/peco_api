f4e7e2ce8679a0b8bc4b9d21e89c917b6b4db1bc
k-nasa <htilcs1115@gmail.com>
2018-11-26 18:34:52 +0900

    $diesel migration run

--- src/schema.rs
+++ src/schema.rs
@@ -8,6 +8,14 @@ table! {
 }
 
 table! {
+    send_emails (id) {
+        id -> Int4,
+        user_id -> Int4,
+        email -> Text,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         token -> Varchar,
@@ -17,8 +25,10 @@ table! {
 }
 
 joinable!(fixed_phrases -> users (user_id));
+joinable!(send_emails -> users (user_id));
 
 allow_tables_to_appear_in_same_query!(
     fixed_phrases,
+    send_emails,
     users,
 );
