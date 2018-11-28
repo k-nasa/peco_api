6fc5115a3c7cfd6871ec56513d1d864eeb115499
k-nasa <htilcs1115@gmail.com>
2018-11-28 13:22:59 +0900

    Add: post_users action test

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,24 @@ mod test {
     use super::rocket;
     use rocket::http::{ContentType, Status};
     use rocket::local::Client;
+
+    #[test]
+    fn post_users() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let mut response = client
+            .post("/users")
+            .header(ContentType::JSON)
+            .body(
+                r#"{
+                  "user_id": "test_user",
+                  "password": "password",
+                  "password_confirmation": "password"
+                  }"#,
+            )
+            .dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type().unwrap(), ContentType::JSON);
+        assert!(response.body_string().unwrap().contains(r#"{"token":"#))
+    }
 }
