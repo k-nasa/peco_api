6afc68b5462ace157bb79b3a482dc3d3775f6db9
k-nasa <htilcs1115@gmail.com>
2018-12-01 17:57:31 +0900

    Delete lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-extern crate peco_api;
-use peco_api::rocket;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate diesel;
+
+pub mod actions;
+pub mod models;
+pub mod schema;
+
+use self::actions::*;
+use crate::rocket::Rocket;
+
+pub fn rocket() -> Rocket {
+    rocket::ignite().mount("/", routes![post_users])
+}
 
 fn main() {
     rocket().launch();
