73091e185540ded97e5f004bb726cb181dd1db1a
k-nasa <htilcs1115@gmail.com>
2018-12-01 19:05:45 +0900

    Test: move to actions module to main

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,37 @@ fn main() {
     rocket().launch();
 }
 
+use self::models::user::*;
+use crate::diesel::{pg::PgConnection, prelude::*};
+use crate::rocket_contrib::json::{Json, JsonValue};
+use dotenv::*;
+use std::env;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().expect("faild load dotenv");
+
+    let databese_url = env::var("DATABASE_URL").expect("DATABASE_URL mut be set");
+
+    PgConnection::establish(&databese_url).expect(&format!("Error connecting to {}", databese_url))
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RequestUser {
+    username: String,
+    password: String,
+    password_confirmation: String,
+}
+
+#[post("/users", format = "application/json", data = "<user>")]
+pub fn post_users(user: Json<RequestUser>) -> JsonValue {
+    let connection = establish_connection();
+
+    // TODO validation and error handle
+    let result_user = User::create_user(&connection, &user.username, &user.password).unwrap();
+
+    json!({ "token": result_user.token })
+}
+
 #[cfg(test)]
 mod test {
     use super::rocket;
